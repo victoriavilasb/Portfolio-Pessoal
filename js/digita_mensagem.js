cdee1bdcc431382d564e5dcd16ebc7d5c2c21ebf
victoriavilasb <vick_vilasb@outlook.com>
2018-10-16 06:57:56 -0300

    Changes final

---
+++ js/digita_mensagem.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	n=0; //número a ser incrementado
+	t = "" //letra incrementada
+	v = 100; //velocidade do texto
+	var texto = "Here is where I tell {almost} everything about myself.";
+
+	function alerta(){
+		t+=texto.charAt(n);
+
+		if(n > texto.length){
+			n=0;
+			t="H"
+		}
+
+		setTimeout(function(){
+			alerta();
+		}, v);
+
+		$('.texto_autocompleta').html(t);
+		n++;
+	}
+
+	setTimeout(function() {
+		alerta();
+	}, v);
+	
+});
